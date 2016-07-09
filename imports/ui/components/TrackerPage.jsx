56f58b95359beb54f567de83f939319ca38c6133
Felipe Milani <felmilani@gmail.com>
2016-07-09 00:52:18 -0300

    Add some style to start/stop button
    Signed-off-by: Felipe Milani <felmilani@gmail.com>

--- imports/ui/components/TrackerPage.jsx
+++ imports/ui/components/TrackerPage.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import moment from 'moment';
 import { i18n } from 'meteor/universe:i18n';
 import ElapsedTimeDisplay from './ElapsedTimeDisplay.jsx';
+import RaisedButton from 'material-ui/RaisedButton';
+import { teal300, teal500, teal700, blueGrey500 } from 'material-ui/styles/colors';
 
 const SECOND = 1000;
-const T = i18n.createComponent(i18n.createTranslator('tracker_page'));
 
 export default class TrackerPage extends React.Component {
 
@@ -22,6 +23,8 @@ export default class TrackerPage extends React.Component {
     this.tick = this.tick.bind(this);
     this.handleClick = this.handleClick.bind(this);
     this.reset = this.reset.bind(this);
+    this.getButtonLabel = this.getButtonLabel.bind(this);
+    this.getButtonColor = this.getButtonColor.bind(this);
   }
 
   componentWillUnmount() {
@@ -78,13 +81,21 @@ export default class TrackerPage extends React.Component {
     });
   }
 
+  getButtonLabel() {
+    return i18n.getTranslation(this.state.running ? 'tracker_page.stop' : 'tracker_page.start');
+  }
+
+  getButtonColor() {
+    return this.state.running ? '#D32F2F' : '#388E3C';
+  }
+
   render() {
-    var style = {
+    const buttonHeight = '50px';
+    const style = {
       button: {
-        fontSize: 20,
-        height: 44,
-        width: 88,
-        margin: 5,
+        width: '100%',
+        marginBottom: '10px',
+        height: buttonHeight,
       },
       record: {
         fontSize: 28,
@@ -93,20 +104,33 @@ export default class TrackerPage extends React.Component {
       }
     };
 
+    const labelStyle = {
+      color: '#fff',
+      fontWeight: 'bold',
+      lineHeight: buttonHeight
+    }
+
     let id=0;
 
     return (
       <div>
         <ElapsedTimeDisplay time={this.state.elapsed} />
-        <button onClick={this.handleClick} style={style.button}>
-          <T>{this.state.running ? 'stop' : 'start'}</T>
-        </button>
-        <button onClick={this.reset} style={style.button}>reset</button>
-        <ul style={style.record}>
-          {this.state.records.map(function(record) {
-            return <li key={id++}>{record.label}</li>;
-          })}
-        </ul>
+        <div style={{ margin: '0 15px' }}>
+          <RaisedButton
+            backgroundColor={this.getButtonColor()}
+            onClick={this.handleClick}
+            style={style.button}
+            label={this.getButtonLabel()}
+            labelStyle={labelStyle}
+          >
+          </RaisedButton>
+          <RaisedButton onClick={this.reset} style={style.button}>reset</RaisedButton>
+          <ul style={style.record}>
+            {this.state.records.map(function(record) {
+              return <li key={id++}>{record.label}</li>;
+            })}
+          </ul>
+        </div>
       </div>
     );
   }

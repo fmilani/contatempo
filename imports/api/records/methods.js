7cd7e379194bdaa6c64198a8097c5fd7c51201e1
Felipe Milani <felmilani@gmail.com>
2016-07-15 22:57:35 -0300

    Add edit and delete record features
    - It is possible to edit a record by clicking/tapping on it
    
    Signed-off-by: Felipe Milani <felmilani@gmail.com>

--- imports/api/records/methods.js
+++ imports/api/records/methods.js
@@ -1,7 +1,12 @@
+import { Meteor } from 'meteor/meteor';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import Records from './records.js';
 import moment from 'moment';
+
+// the precision with which the record dates are saved. Any unit below this will be zero
+const PRECISION = 'seconds';
+
 /**
  * Inserts a record on the database
  * @param {Date} begin - the date that the record begun
@@ -34,8 +39,62 @@ export const complete = new ValidatedMethod({
   run({ id, end }) {
     Records.update(id, {
       $set: {
-        end: moment(end).startOf('seconds').toDate(),
+        end: moment(end).startOf(PRECISION).toDate(),
       },
     });
+  },
+});
+
+/**
+ * Edit the given field (begin or end) of the record with the given id
+ * @param {String} id - the id of the record
+ * @param {String} field - the name of the field to be edited
+ * @param {Date} date - the new date value of that field
+ */
+export const edit = new ValidatedMethod({
+  name: 'records.edit',
+  validate: new SimpleSchema({
+    id: { type: String },
+    field: { type: String, allowedValues: ['begin', 'end'] },
+    date: { type: Date },
+  }).validator(),
+  run({ id, field, date }) {
+    const record = Records.findOne(id);
+
+    if (field === 'begin') {
+      if (moment(date).isAfter(moment(record.end))) {
+        throw new Meteor.Error('records.edit.beginAfterEnd',
+          'Cannot edit record\'s begin to a time after its end');
+      }
+      Records.update(id, {
+        $set: {
+          begin: moment(date).startOf(PRECISION).toDate(),
+        },
+      });
+    } else {
+      if (moment(date).isBefore(moment(record.begin))) {
+        throw new Meteor.Error('records.edit.endBeforeBegin',
+        'Cannot edit record\'s end to a time before its begin');
+      }
+      Records.update(id, {
+        $set: {
+          end: moment(date).startOf(PRECISION).toDate(),
+        },
+      });
+    }
+  },
+});
+
+/**
+ * Remove the record with the given id from the database
+ * @param {String} id - the id of the record
+ */
+export const remove = new ValidatedMethod({
+  name: 'records.remove',
+  validate: new SimpleSchema({
+    id: { type: String },
+  }).validator(),
+  run({ id }) {
+    Records.remove(id);
   },
 });
--- imports/ui/components/RecordsList.jsx
+++ imports/ui/components/RecordsList.jsx
@@ -1,8 +1,13 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import moment from 'moment';
 import { i18n } from 'meteor/universe:i18n';
-import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn }
-  from 'material-ui/Table';
+import TimePicker from 'material-ui/TimePicker';
+import { List } from 'material-ui/List';
+import IconButton from 'material-ui/IconButton';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
+import Divider from 'material-ui/Divider';
+import { edit, remove } from '../../api/records/methods.js';
 
 /**
  * Shows time records in a table (yes, not a list).
@@ -17,6 +22,7 @@ export default class RecordsList extends React.Component {
     super(props);
 
     this.t = i18n.createTranslator('records');
+    this.editRecord = this.editRecord.bind(this);
   }
 
   // formats a record time with the hours, minutes and seconds (HH:mm:ss)
@@ -24,47 +30,86 @@ export default class RecordsList extends React.Component {
     return recordTime ? moment(recordTime).format('HH:mm:ss') : '...';
   }
 
+  editRecord(record, field, date) {
+    if (field === 'begin') {
+      // TODO: refactor this verification as it is the same one in the server
+      if (moment(date).isAfter(moment(record.end))) {
+        this.refs[`${record._id}_${field}`].setState({ time: record[field] });
+        throw new Meteor.Error('records.edit.beginAfterEnd',
+          'Cannot edit record\'s begin to a time after its end');
+      }
+    } else {
+      if (moment(date).isBefore(moment(record.begin))) {
+        this.refs[`${record._id}_${field}`].setState({ time: record[field] });
+        throw new Meteor.Error('records.edit.endBeforeBegin',
+        'Cannot edit record\'s end to a time before its begin');
+      }
+    }
+    edit.call({
+      id: record._id,
+      field,
+      date,
+    });
+  }
+
+  removeRecord(id) {
+    remove.call({ id });
+  }
+
   render() {
+    const grey500 = '#9E9E9E'; // TODO: use palette (grey500)
     return (
-      <Table selectable={false}>
-        <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
-          <TableRow>
-            <TableHeaderColumn
-              colSpan="2"
-              tooltip={this.t('records_tooltip')}
-              style={{ textAlign: 'center' }}
-            >
-              {this.t('records')}
-            </TableHeaderColumn>
-          </TableRow>
-          <TableRow>
-            <TableHeaderColumn
-              style={{ textAlign: 'center' }}
-              tooltip={this.t('begin_tooltip')}
-            >
+      <div>
+        <List>
+          <div style={{ display: 'flex', margin: '15px 0px', color: grey500, fontSize: '12px' }}>
+            <div style={{ flex: '0 0 40%', textAlign: 'center' }}>
               {this.t('begin')}
-            </TableHeaderColumn>
-            <TableHeaderColumn
-              style={{ textAlign: 'center' }}
-              tooltip={this.t('end_tooltip')}
-            >
+            </div>
+            <div style={{ flex: '0 0 40%', textAlign: 'center' }}>
               {this.t('end')}
-            </TableHeaderColumn>
-          </TableRow>
-        </TableHeader>
-        <TableBody displayRowCheckbox={false}>
-          {this.props.records.map((record, index) => (
-            <TableRow key={index}>
-              <TableRowColumn style={{ textAlign: 'center' }}>
-                {this.formatRecordTime(record.begin)}
-              </TableRowColumn>
-              <TableRowColumn style={{ textAlign: 'center' }}>
-                {this.formatRecordTime(record.end)}
-              </TableRowColumn>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+            </div>
+          </div>
+            {this.props.records.map((record, index) => (
+              <div key={index}>
+                <Divider />
+                <div style={{ display: 'flex' }}>
+                  <div style={{ flex: '0 0 40%', textAlign: 'center' }}>
+                    <TimePicker
+                      ref={`${record._id}_begin`}
+                      textFieldStyle={{ width: '100%' }}
+                      inputStyle={{ textAlign: 'center', fontSize: '13px' }}
+                      underlineShow={false}
+                      format="24hr"
+                      name="recordTime"
+                      value={record.begin}
+                      onChange={(event, date) => { this.editRecord(record, 'begin', date); }}
+                    />
+                  </div>
+                  <div style={{ flex: '0 0 40%', textAlign: 'center' }}>
+                    <TimePicker
+                      ref={`${record._id}_end`}
+                      textFieldStyle={{ width: '100%' }}
+                      inputStyle={{ textAlign: 'center', fontSize: '13px' }}
+                      underlineShow={false}
+                      format="24hr"
+                      name="recordTime"
+                      value={record.end}
+                      onChange={(event, date) => { this.editRecord(record, 'end', date); }}
+                    />
+                  </div>
+                  <div style={{ flex: '0 0 20%', textAlign: 'center' }}>
+                    <IconButton
+                      onClick={() => { this.removeRecord(record._id); }}
+                      iconStyle={{ color: grey500, width: '20px', height: '20px' }}
+                    >
+                      <ActionDelete />
+                    </IconButton>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </List>
+      </div>
     );
   }
 }

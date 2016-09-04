150170f53da42ccc7f0c0adedc955519816e9fa5
Felipe Milani <felmilani@gmail.com>
2016-09-04 18:25:45 -0300

    Change month interval
    - Now it starts on day 21 of previous month and ends on day 20
      of the current one.
    
    Signed-off-by: Felipe Milani <felmilani@gmail.com>

--- imports/containers/TrackerPageContainer.jsx
+++ imports/containers/TrackerPageContainer.jsx
@@ -5,17 +5,52 @@ import Records from '../api/records/records.js';
 import moment from 'moment';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+/**
+ * Gets the day interval of the given date
+ * @param {Date} date - the given date
+ * @return {Object} - an object representing the interval with start and end dates
+ */
+const getDayInterval = (date) => {
+  const ref = moment(date);
+
+  const start = moment(ref).startOf('day');
+  const end = moment(ref).endOf('day');
+
+  return { start, end };
+};
+
+/**
+ * Gets the month interval of the given date
+ * @param {Date} date - the given date
+ * @return {Object} - an object representing the interval with start and end dates
+ */
+const getMonthInterval = (date) => {
+  const ref = moment(date);
+  const currentMonth = moment(ref).subtract(20, 'days').add(1, 'month')
+    .month();
+  const start = moment(ref).month(currentMonth - 1).date(21)
+    .startOf('day');
+  const end = moment(ref).month(currentMonth).date(20)
+    .endOf('day');
+
+  return { start, end };
+};
 
 export default createContainer(({ params }) => {
   const period = params.period || 'day';
+
+  let getInterval = getDayInterval;
+  if (period === 'month') {
+    getInterval = getMonthInterval;
+  }
   // reactive var to be update by the TrackerPage component in order to update the subscription
   // according to params
   const checkSubscriptionInterval = new ReactiveVar(moment(), (oldValue, newValue) => (
-    oldValue.startOf(period).isSame(newValue.startOf(period))
+    getInterval(oldValue).start.isSame(getInterval(newValue).start)
   ));
   const subscriptionInterval = moment(checkSubscriptionInterval.get());
-  const startInterval = subscriptionInterval.startOf(period).toDate();
-  const endInterval = subscriptionInterval.endOf(period).toDate();
+  const startInterval = getInterval(subscriptionInterval).start.toDate();
+  const endInterval = getInterval(subscriptionInterval).end.toDate();
 
   const recordsHandle = Meteor.subscribe('records.interval', {
     start: startInterval,

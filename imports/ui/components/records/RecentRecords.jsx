105a47bd1b05039b9072a882f966dc3e0f794972
Felipe Milani <felmilani@gmail.com>
2017-04-29 20:32:23 -0300

    Create RecentRecords component
    - And its container
    
    Signed-off-by: Felipe Milani <felmilani@gmail.com>

---
+++ imports/ui/components/records/RecentRecords.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { i18n } from 'meteor/universe:i18n';
+import RecordsList from './RecordsList.jsx';
+
+/**
+ * Component that shows the list of the recent records
+ */
+const RecentRecords = props => (
+  <RecordsList
+    records={props.recentRecords}
+    title={i18n.getTranslation('common.last_days')}
+  />
+);
+
+RecentRecords.propTypes = {
+  recentRecords: React.PropTypes.arrayOf(React.PropTypes.shape({
+    begin: React.PropTypes.instanceOf(Date),
+    end: React.PropTypes.instanceOf(Date),
+  })),
+};
+
+RecentRecords.defaultProps = {
+  recentRecords: [],
+};
+
+export default RecentRecords;

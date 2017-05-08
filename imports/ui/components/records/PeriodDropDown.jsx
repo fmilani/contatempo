1618176fa1869fe57c83b88de384a9d043d7516c
Felipe Milani <felmilani@gmail.com>
2017-05-07 22:14:15 -0300

    Create PeriodDropDown component
    Signed-off-by: Felipe Milani <felmilani@gmail.com>

---
+++ imports/ui/components/records/PeriodDropDown.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import { i18n } from 'meteor/universe:i18n';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import URLS from '../../../api/helpers/urls.js';
+
+/**
+ * Component for a list of history periods.
+ */
+class PeriodDropDown extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSelectPeriod = this.onSelectPeriod.bind(this);
+  }
+
+  onSelectPeriod(event, index, value) {
+    this.props.router.push(value);
+  }
+
+  render() {
+    return (
+      <DropDownMenu
+        style={{ height: '64px' }}
+        iconStyle={{ marginTop: '-2px', marginRight: '2px' }}
+        underlineStyle={{ display: 'none' }}
+        labelStyle={{
+          color: 'white',
+          fontSize: 20,
+          paddingLeft: 0,
+          height: '64px',
+          lineHeight: '64px',
+        }}
+        value={this.props.location.pathname}
+        onChange={this.onSelectPeriod}
+      >
+        <MenuItem
+          value={URLS.HISTORY.THIS_WEEK}
+          primaryText={i18n.getTranslation('period.this_week')}
+        />
+        <MenuItem
+          value={URLS.HISTORY.THIS_MONTH}
+          primaryText={i18n.getTranslation('period.this_month')}
+        />
+        <MenuItem
+          value={URLS.HISTORY.LAST_WEEK}
+          primaryText={i18n.getTranslation('period.last_week')}
+        />
+        <MenuItem
+          value={URLS.HISTORY.LAST_MONTH}
+          primaryText={i18n.getTranslation('period.last_month')}
+        />
+      </DropDownMenu>
+    );
+  }
+}
+
+PeriodDropDown.propTypes = {
+  location: React.PropTypes.shape({
+    pathname: React.PropTypes.string,
+  }).isRequired,
+  router: React.PropTypes.shape({
+    push: React.PropTypes.func,
+  }).isRequired,
+};
+
+export default withRouter(PeriodDropDown);

6af7c2e2e652b19b0018d52bfd0507631b708abf
Felipe Milani <felmilani@gmail.com>
2017-05-10 23:16:36 -0300

    Create HistoryPage component
     - And remove TrackerPage
    
    Signed-off-by: Felipe Milani <felmilani@gmail.com>

---
+++ imports/ui/components/HistoryPage.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import moment from 'moment';
+import { i18n } from 'meteor/universe:i18n';
+import BigTimeDisplay from './records/BigTimeDisplay.jsx';
+import RecordsList from './records/RecordsList.jsx';
+import Spinner from './Spinner.jsx';
+
+/**
+ * Component to display the user's history of records
+ */
+export default class HistoryPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    // bindings
+    this.getTotalElapsed = this.getTotalElapsed.bind(this);
+  }
+
+  getTotalElapsed() {
+    const finishedElapsed = this.props.records
+      .map(record => moment(record.end).diff(moment(record.begin)))
+      .reduce((l, n) => l + n, 0);
+    return finishedElapsed;
+  }
+
+  render() {
+    const { loading } = this.props;
+    return (
+      <div>
+        <BigTimeDisplay
+          time={this.getTotalElapsed()}
+          title={i18n.getTranslation('common.total')}
+        />
+        <div style={{ marginTop: 15 }}>
+          {
+            loading
+              ? <Spinner />
+              : <RecordsList records={this.props.records} />
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+HistoryPage.propTypes = {
+  loading: React.PropTypes.bool,
+  records: React.PropTypes.arrayOf(React.PropTypes.shape({
+    begin: React.PropTypes.instanceOf(Date),
+    end: React.PropTypes.instanceOf(Date),
+  })),
+};
+
+HistoryPage.defaultProps = {
+  loading: false,
+  records: [],
+};
+
+HistoryPage.contextTypes = {
+  currentUser: React.PropTypes.object,
+};

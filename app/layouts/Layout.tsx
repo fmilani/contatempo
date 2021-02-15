f656ac7696aee15a0506917e2476620e1641a14a
Felipe Milani <felmilani@gmail.com>
2021-02-15 16:05:50 -0300

    Remove boilerplate and styling from index page

--- app/layouts/Layout.tsx
+++ app/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react"
 import { Head } from "blitz"
+import { Container } from "@chakra-ui/react"
 
 type LayoutProps = {
   title?: string
@@ -14,7 +15,7 @@ const Layout = ({ title, children }: LayoutProps) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {children}
+      <Container centerContent>{children}</Container>
     </>
   )
 }

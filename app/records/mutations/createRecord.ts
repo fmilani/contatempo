c93f042879bdf03556a6773e7e44aa14042a47a6
Felipe Milani <felmilani@gmail.com>
2021-03-04 00:13:28 -0300

    Truncate seconds and millis on createRecord

--- app/records/mutations/createRecord.ts
+++ app/records/mutations/createRecord.ts
@@ -1,11 +1,23 @@
 import { Ctx } from "blitz"
+import { set } from "date-fns"
 import db, { Prisma } from "db"
 
 type CreateRecordInput = Pick<Prisma.RecordCreateArgs, "data">
 export default async function createRecord({ data }: CreateRecordInput, ctx: Ctx) {
   ctx.session.$authorize()
 
-  const record = await db.record.create({ data })
+  const startRounded = set(new Date(data.start), { seconds: 0, milliseconds: 0 })
+
+  let finishRounded: Date | null = null
+  if (data.finish) finishRounded = set(new Date(data.finish), { seconds: 0, milliseconds: 0 })
+
+  const record = await db.record.create({
+    data: {
+      ...data,
+      start: startRounded,
+      finish: finishRounded,
+    },
+  })
 
   return record
 }

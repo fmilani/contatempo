fe2e28f07924ddb8c0e0aa22e2501bd49a170fc0
Felipe Milani <felmilani@gmail.com>
2021-03-06 16:56:18 -0300

    Truncate seconds and millis on updateRecord

--- app/records/mutations/updateRecord.ts
+++ app/records/mutations/updateRecord.ts
@@ -1,4 +1,5 @@
 import { Ctx } from "blitz"
+import { set } from "date-fns"
 import db, { Prisma } from "db"
 
 type UpdateRecordInput = Pick<Prisma.RecordUpdateArgs, "where" | "data">
@@ -6,7 +7,21 @@ type UpdateRecordInput = Pick<Prisma.RecordUpdateArgs, "where" | "data">
 export default async function updateRecord({ where, data }: UpdateRecordInput, ctx: Ctx) {
   ctx.session.$authorize()
 
-  const record = await db.record.update({ where, data })
+  let startRounded: Date | undefined
+  if (data.start)
+    startRounded = set(new Date(data.start.toString()), { seconds: 0, milliseconds: 0 })
+
+  let finishRounded: Date | undefined
+  if (data.finish)
+    finishRounded = set(new Date(data.finish.toString()), { seconds: 0, milliseconds: 0 })
+  const record = await db.record.update({
+    where,
+    data: {
+      ...data,
+      start: startRounded,
+      finish: finishRounded,
+    },
+  })
 
   return record
 }

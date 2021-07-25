a0abd4f15321913cd73e0a23744b89f7721bbe1b
Felipe Milani <felmilani@gmail.com>
2021-07-25 00:21:19 -0300

    RecordForm prototype with react-datepicker

--- app/records/components/RecordForm.tsx
+++ app/records/components/RecordForm.tsx
@@ -1,12 +1,33 @@
 import { Form, FormProps } from "app/core/components/Form"
-import { LabeledTextField } from "app/core/components/LabeledTextField"
+import { useField } from "react-final-form"
 import { z } from "zod"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
+
 export { FORM_ERROR } from "app/core/components/Form"
 
 export function RecordForm<S extends z.ZodType<any, any>>(props: FormProps<S>) {
+  function CustomDatePicker(props: { name: string }) {
+    const { input } = useField(props.name)
+    const ExampleCustomTimeInput = ({ value, onChange }) => (
+      <input type="time" value={value} onChange={(e) => onChange(e.target.value)} />
+    )
+    return (
+      <DatePicker
+        dateFormat="dd/MM/yyyy HH:mm"
+        selected={input.value}
+        onChange={input.onChange}
+        showTimeInput
+        customTimeInput={<ExampleCustomTimeInput />}
+      />
+    )
+  }
   return (
     <Form<S> {...props}>
-      <LabeledTextField name="name" label="Name" placeholder="Name" />
+      <label htmlFor="begin">Begin</label>
+      <CustomDatePicker name="begin" />
+      <label htmlFor="end">End</label>
+      <CustomDatePicker name="end" />
     </Form>
   )
 }
--- app/records/mutations/createRecord.ts
+++ app/records/mutations/createRecord.ts
@@ -3,12 +3,19 @@ import db from "db"
 import { z } from "zod"
 
 const CreateRecord = z.object({
-  name: z.string(),
+  begin: z.date(),
+  end: z.date(),
 })
 
-export default resolver.pipe(resolver.zod(CreateRecord), resolver.authorize(), async (input) => {
-  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
-  const record = await db.record.create({ data: input })
+export default resolver.pipe(
+  resolver.zod(CreateRecord),
+  resolver.authorize(),
+  async (input, { session }) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    const record = await db.record.create({
+      data: { ...input, user: { connect: { id: session.userId } } },
+    })
 
-  return record
-})
+    return record
+  }
+)

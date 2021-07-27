9bc2eed84d5c241120e0864b5c460295b668f37a
Felipe Milani <felmilani@gmail.com>
2021-07-26 23:13:58 -0300

    Group records by date

--- app/pages/records/index.tsx
+++ app/pages/records/index.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react"
 import { Head, Link, usePaginatedQuery, useRouter, BlitzPage, Routes } from "blitz"
 import Layout from "app/core/layouts/Layout"
 import getRecords from "app/records/queries/getRecords"
+import { Record } from "db"
 
 const ITEMS_PER_PAGE = 100
 
@@ -9,7 +10,7 @@ export const RecordsList = () => {
   const router = useRouter()
   const page = Number(router.query.page) || 0
   const [{ records, hasMore }] = usePaginatedQuery(getRecords, {
-    orderBy: { id: "asc" },
+    orderBy: { begin: "desc" },
     skip: ITEMS_PER_PAGE * page,
     take: ITEMS_PER_PAGE,
   })
@@ -17,20 +18,38 @@ export const RecordsList = () => {
   const goToPreviousPage = () => router.push({ query: { page: page - 1 } })
   const goToNextPage = () => router.push({ query: { page: page + 1 } })
 
+  function groupBy(propertyExtractor: (r: Record) => string) {
+    return function group(array: Record[]) {
+      return array.reduce((acc, obj) => {
+        const property = propertyExtractor(obj)
+        acc[property] = acc[property] || []
+        acc[property].push(obj)
+        return acc
+      }, {})
+    }
+  }
+
+  const groupByDay = groupBy(
+    (record) =>
+      `${record.begin.getFullYear()}-${record.begin.getMonth() + 1}-${record.begin.getDate()}`
+  )
+
   return (
     <div>
-      <ul>
-        {records.map((record) => (
-          <li key={record.id}>
-            <Link href={Routes.ShowRecordPage({ recordId: record.id })}>
-              <a>
-                {record.begin.toString()} - {record.end?.toString()}
-              </a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-
+      {Object.entries(groupByDay(records)).map(([day, records]) => (
+        <ul key={day}>
+          <div>{day}</div>
+          {(records as Record[]).reverse().map((record: Record) => (
+            <li key={record.id}>
+              <Link href={Routes.ShowRecordPage({ recordId: record.id })}>
+                <a>
+                  {record.begin.toString()} - {record.end?.toString()}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      ))}
       <button disabled={page === 0} onClick={goToPreviousPage}>
         Previous
       </button>

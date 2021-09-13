83083374a8faa7770727642d570620bea2477bf0
Felipe Milani <felmilani@gmail.com>
2021-09-13 15:54:03 -0300

    Add a header, theme and layout

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,9 +1,54 @@
-import { ReactNode } from "react"
-import { Head } from "blitz"
+import React, { ReactNode, useEffect, useRef, useState } from "react"
+import { Head, Link } from "blitz"
+import { Container, Flex, Heading } from "@chakra-ui/layout"
+import { chakra, HStack, Spacer } from "@chakra-ui/react"
+import { useViewportScroll } from "framer-motion"
 
 type LayoutProps = {
   title?: string
   children: ReactNode
+}
+
+const Header = () => {
+  const ref = useRef<HTMLHeadingElement>(null)
+  const [y, setY] = useState(0)
+  const { height = 0 } = ref.current?.getBoundingClientRect() ?? {}
+
+  const { scrollY } = useViewportScroll()
+  useEffect(() => {
+    return scrollY.onChange(() => setY(scrollY.get()))
+  }, [scrollY])
+
+  return (
+    <chakra.header
+      ref={ref}
+      shadow={y > height ? "md" : undefined}
+      transition="box-shadow 0.2s"
+      bg="white"
+      pos="sticky"
+      width="full"
+      borderTop="4px solid"
+      borderTopColor="brand.500"
+      top="0"
+      left="0"
+      right="0"
+      zIndex="999"
+    >
+      <Container height="4.5rem" display="flex" alignItems="center">
+        <Link href="/" passHref>
+          <chakra.a aria-label="Contatempo, Back to homepage">
+            <Heading size="md">Contatempo</Heading>
+          </chakra.a>
+        </Link>
+        <Spacer />
+        <HStack spacing="4">
+          <Link href="/records" passHref>
+            <chakra.a aria-label="Navigate to records page">Records</chakra.a>
+          </Link>
+        </HStack>
+      </Container>
+    </chakra.header>
+  )
 }
 
 const Layout = ({ title, children }: LayoutProps) => {
@@ -13,8 +58,8 @@ const Layout = ({ title, children }: LayoutProps) => {
         <title>{title || "contatempo"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      {children}
+      <Header />
+      <Container>{children}</Container>
     </>
   )
 }
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -8,14 +8,44 @@ import {
   useQueryErrorResetBoundary,
 } from "blitz"
 import LoginForm from "app/auth/components/LoginForm"
+import { ChakraProvider, theme as defaultTheme, extendTheme } from "@chakra-ui/react"
 
-import { ChakraProvider } from "@chakra-ui/react"
-
+console.log(defaultTheme)
+const theme = extendTheme({
+  colors: {
+    brand: {
+      "50": "#ECEEF8",
+      "100": "#CACFED",
+      "200": "#A8B1E1",
+      "300": "#8692D5",
+      "400": "#6473C9",
+      "500": "#4255BD",
+      "600": "#354497",
+      "700": "#283371",
+      "800": "#1A224C",
+      "900": "#0D1126",
+    },
+  },
+  components: {
+    Container: {
+      baseStyle: {
+        maxW: "80ch",
+      },
+    },
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.100",
+      },
+    },
+  },
+})
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ErrorBoundary
         FallbackComponent={RootErrorFallback}
         onReset={useQueryErrorResetBoundary().reset}

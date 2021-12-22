7a7ee0445d3b6b2ff11e423f9fdf26175ba02b8e
Felipe Milani <felmilani@gmail.com>
2021-12-22 00:10:37 -0300

    Clean Slate for Remix

--- imports/startup/server/register-api.js
+++
@@ -1,8 +0,0 @@
-// register all api run on server-side
-
-import '../../api/plans/methods.js';
-import '../../api/plans/server/publications.js';
-import '../../api/records/methods.js';
-import '../../api/records/server/publications.js';
-import '../../api/settings/server/publications.js';
-import '../../api/users/server/publications.js';

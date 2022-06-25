eb638e42e322f7896c612909af33f36ff26c08aa
Felipe Milani <felmilani@gmail.com>
2022-06-25 20:38:21 -0300

    clean slate

--- imports/api/settings/EndOfMonthEnum.js
+++
@@ -1,9 +0,0 @@
-/**
- * The enum with options for users' end of month
- */
-const EndOfMonthEnum = {
-  LAST_DAY: 'LAST_DAY',
-  DAY_20: 'DAY_20',
-};
-
-export default EndOfMonthEnum;
--- imports/api/settings/server/publications.js
+++
@@ -1,7 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-
-// publish app settings of a given user
-Meteor.publish('user.settings', function userSettings() {
-  // return only the settings field from the logged user
-  return Meteor.users.find({ _id: this.userId }, { fields: { settings: 1 } });
-});

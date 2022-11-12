1391246a2a20d02ed3d565350a479e0a37482050
Felipe Milani <felmilani@gmail.com>
2022-11-12 20:00:02 -0300

    Create api file to handle backend requests

--- apps/contatempo/app/dashboard/page.tsx
+++ apps/contatempo/app/dashboard/page.tsx
@@ -1,3 +1,20 @@
-export default function DashboardPage() {
-  return <div>Dashboard Page</div>;
+import { getRecords } from "../../lib/api";
+
+export default async function DashboardPage() {
+  const records = await getRecords({
+    from: new Date(2020, 1, 1),
+    to: new Date(2023, 2, 1),
+  });
+  return (
+    <div>
+      <p>DashboardPage</p>
+      <ul>
+        {records.map((record) => (
+          <li key={record.id}>
+            {record.begin} - {record.end}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
---
+++ apps/contatempo/lib/api.ts
@@ -0,0 +1,25 @@
+import { getSession } from "./session";
+
+type Record = {
+  id: string;
+  begin: string;
+  end: string;
+};
+
+interface GetRecordsRequest {
+  from: Date;
+  to: Date;
+}
+
+export async function getRecords({ from, to }: GetRecordsRequest) {
+  const session = await getSession();
+  const records: Record[] = await fetch(
+    `${
+      process.env.BACKEND_URL
+    }/records?from=${from.toISOString()}&to=${to.toISOString()}`,
+    {
+      headers: { Authorization: `Bearer ${session?.accessToken}` },
+    }
+  ).then((r) => r.json());
+  return records;
+}

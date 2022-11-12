da0cc2a1afdca5f3bd694a7cbd95206a67c506a3
Felipe Milani <felmilani@gmail.com>
2022-11-12 17:10:35 -0300

    Redirect to /dashboard from / if user logged in

--- apps/contatempo/app/page.tsx
+++ apps/contatempo/app/page.tsx
@@ -1,6 +1,13 @@
+import { redirect } from "next/navigation";
 import LoginButton from "../components/LoginButton";
+import { getCurrentUser } from "../lib/session";
 
-export default function Home() {
+export default async function Home() {
+  const user = await getCurrentUser();
+  if (user) {
+    return redirect("/dashboard");
+  }
+
   return (
     <div>
       <h1>Contatempo</h1>

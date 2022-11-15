79378737b7797d2b5389051be79c36d2cd74dc0a
Felipe Milani <felmilani@gmail.com>
2022-11-15 17:25:17 -0300

    Group records of the same day

--- apps/contatempo/app/(user)/records/page.tsx
+++ apps/contatempo/app/(user)/records/page.tsx
@@ -1,6 +1,17 @@
 import CurrentRecord from "@/components/CurrentRecord";
-import { getRecords } from "@/lib/api";
+import { getRecords, Record } from "@/lib/api";
+import format from "date-fns/format";
+import ptbr from "date-fns/locale/pt-BR";
+import parse from "date-fns/parse";
 
+function groupRecords(records: Record[]): { [key: string]: Record[] } {
+  return records.reduce((acc, record) => {
+    const day = format(new Date(record.begin), "yyyy-MM-dd");
+    acc[day] = acc[day] ?? [];
+    acc[day].push(record);
+    return acc;
+  }, {});
+}
 export default async function RecordsPage() {
   const records = await getRecords({
     from: new Date(2020, 1, 1),
@@ -9,16 +20,28 @@ export default async function RecordsPage() {
   return (
     <div>
       <div className="flex items-center justify-between">
-        <span className="text-xl font-bold">Records</span>
+        <span className="text-xl font-bold">Registros</span>
         <CurrentRecord record={records.find((r) => !r.end)} />
       </div>
-      <ul className="mt-8">
-        {records.map((record) => (
-          <li key={record.id}>
-            {record.begin} - {record.end}
-          </li>
+      <div className="mt-8 space-y-4">
+        {Object.entries(groupRecords(records)).map(([day, recordsOfDay]) => (
+          <div key={day}>
+            <div className="mb-2">
+              {format(parse(day, "yyyy-MM-dd", new Date()), "dd 'de' MMMM", {
+                locale: ptbr,
+              })}
+            </div>
+            <ul className="rounded-lg drop-shadow-sm bg-white divide divide-y">
+              {recordsOfDay.map((record) => (
+                <li className="p-4" key={record.id}>
+                  {format(new Date(record.begin), "HH:mm:ss")} -{" "}
+                  {format(new Date(record.end), "HH:mm:ss")}
+                </li>
+              ))}
+            </ul>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }

293bdac96c318f40b87f26d43a549606ed15a7e2
Felipe Milani <felmilani@gmail.com>
2022-11-15 21:40:22 -0300

    Use middleware to redirect logged user from / to /dashboard

--- apps/contatempo/app/(landing)/page.tsx
+++ apps/contatempo/app/(landing)/page.tsx
@@ -1,13 +1,6 @@
-import { redirect } from "next/navigation";
 import LoginButton from "@/components/LoginButton";
-import { getCurrentUser } from "@/lib/session";
 
 export default async function Home() {
-  const user = await getCurrentUser();
-  if (user) {
-    redirect("/dashboard");
-  }
-
   return (
     <div>
       <h1>Contatempo</h1>

369218655faedfe2da272e20d9503b3044dee995
Felipe Milani <felmilani@gmail.com>
2024-01-07 16:00:44 -0300

    change min-h to h in layout

--- apps/contatempo/app/layout.tsx
+++ apps/contatempo/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html className="antialiased bg-gray-100 text-gray-700/[.87]">
       <head />
-      <body><div vaul-drawer-wrapper="" className="bg-gray-100 min-h-[100svh]">{children}</div></body>
+      <body><div vaul-drawer-wrapper="" className="bg-gray-100 h-[100svh]">{children}</div></body>
     </html>
   );
 }

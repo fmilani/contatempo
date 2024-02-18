5a752f5403d0386e115940ba7d73af6b3be352fb
Felipe Milani <felmilani@gmail.com>
2024-02-18 18:31:08 -0300

    Add more sizes to the favicon

--- apps/contatempo/app/icon.tsx
+++ apps/contatempo/app/icon.tsx
@@ -1,42 +1,49 @@
 import React from 'react'
 import { ImageResponse } from '@vercel/og'
  
-// Route segment config
 export const runtime = 'edge'
  
-// Image metadata
-export const size = {
-  width: 32,
-  height: 32,
+const imageSize = {
+  width: 192,
+  height: 192,
 }
-export const contentType = 'image/png'
- 
-// Image generation
+
+export function generateImageMetadata() {
+  return [
+    {
+      contentType: 'image/png',
+      size: { width: 48, height: 48 },
+      id: 'small',
+    },
+    {
+      contentType: 'image/png',
+      size: { width: 72, height: 72 },
+      id: 'medium',
+    },
+    {
+      contentType: 'image/png',
+      size: imageSize,
+      id: 'large',
+    },
+  ]
+}
+
 export default function Icon() {
   return new ImageResponse(
-    (
-      // ImageResponse JSX element
-      <div
-        style={{
-          fontSize: 28,
-          background: 'black',
-          width: '100%',
-          height: '100%',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          borderRadius: 9999,
-          color: 'white',
-        }}
-      >
-        ct
-      </div>
-    ),
-    // ImageResponse options
-    {
-      // For convenience, we can re-use the exported icons size metadata
-      // config to also set the ImageResponse's width and height.
-      ...size,
-    }
+    <div
+      style={{
+        fontSize: 164,
+        background: 'black',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 9999,
+        color: 'white',
+      }}
+    >
+      ct
+    </div>, {...imageSize}
   )
 }

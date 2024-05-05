5a53636c508663ed27ff04b2e5d2d51ab8680277
Felipe Milani <felmilani@gmail.com>
2024-05-05 16:19:21 -0300

    add tags to Record type

--- apps/contatempo/lib/api.ts
+++ apps/contatempo/lib/api.ts
@@ -1,39 +1,40 @@
-import { getSession } from "./session";
+import { getSession } from "./session"
 
 export type Record = {
-  id: string;
-  begin: string;
-  end?: string;
-};
+  id: string
+  begin: string
+  end?: string
+  tags: { id: string; value: string }[]
+}
 
 interface GetRecordsRequest {
-  from: Date;
-  to: Date;
+  from: Date
+  to: Date
 }
 
 export async function getRecords({ from, to }: GetRecordsRequest) {
-  const session = await getSession();
+  const session = await getSession()
   const records: Record[] = await fetch(
     `${
       process.env.BACKEND_URL
     }/records?from=${from.toISOString()}&to=${to.toISOString()}`,
     {
       headers: { Authorization: `Bearer ${session?.accessToken}` },
-    }
-  ).then((r) => r.json());
-  return records;
+    },
+  ).then((r) => r.json())
+  return records
 }
 
 export async function getSummary({ from, to }: GetRecordsRequest) {
-  const session = await getSession();
+  const session = await getSession()
   const summary = await fetch(
     `${
       process.env.BACKEND_URL
     }/records/summary?from=${from.toISOString()}&to=${to.toISOString()}`,
     {
       headers: { Authorization: `Bearer ${session?.accessToken}` },
-    }
-  ).then((r) => r.json());
+    },
+  ).then((r) => r.json())
 
-  return summary.value;
+  return summary.value
 }

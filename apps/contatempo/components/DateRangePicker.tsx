fc77efff45c815b11a1a3222b7d46321a25ae665
Felipe Milani <felmilani@gmail.com>
2024-05-12 22:30:10 -0300

    range options on DateRangePicker

--- apps/contatempo/components/DateRangePicker.tsx
+++ apps/contatempo/components/DateRangePicker.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 import { format } from "date-fns"
 import { Calendar as CalendarIcon } from "lucide-react"
 import { DateRange } from "react-day-picker"
-import { cn } from "@/lib/utils"
+import { cn, getRanges } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Calendar } from "@/components/ui/calendar"
 import { Badge } from "@/components/ui/badge"
@@ -14,6 +14,13 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover"
 import { useMediaQuery } from "@/hooks/useMediaQuery"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select"
 
 interface DateRangePickerProps {
   initialRange: DateRange
@@ -28,6 +35,7 @@ export function DateRangePicker({
   const [date, setDate] = React.useState<DateRange | undefined>(initialRange)
   const [open, setOpen] = React.useState(false)
 
+  const ranges = getRanges()
   return (
     <Popover
       open={open}
@@ -57,15 +65,43 @@ export function DateRangePicker({
           )}
         </Badge>
       </PopoverTrigger>
-      <PopoverContent className="w-auto p-0" align="start">
-        <Calendar
-          initialFocus
-          mode="range"
-          defaultMonth={date?.from}
-          selected={date}
-          onSelect={setDate}
-          numberOfMonths={isDesktop ? 2 : 1}
-        />
+      <PopoverContent className="w-auto flex flex-col gap-2 p-2" align="start">
+        <Select
+          onValueChange={(v) => {
+            const value = JSON.parse(v)
+            const range = {
+              from: new Date(value.from),
+              to: new Date(value.to),
+            }
+            setDate(range)
+          }}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Choose a period" />
+          </SelectTrigger>
+          <SelectContent position="popper">
+            <SelectItem value={JSON.stringify(ranges.today)}>Today</SelectItem>
+            <SelectItem value={JSON.stringify(ranges.thisWeek)}>
+              This week
+            </SelectItem>
+            <SelectItem value={JSON.stringify(ranges.thisMonth)}>
+              This month
+            </SelectItem>
+            <SelectItem value={JSON.stringify(ranges.lastMonth)}>
+              Last month
+            </SelectItem>
+          </SelectContent>
+        </Select>
+        <div className="rounded-md border">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={setDate}
+            numberOfMonths={isDesktop ? 2 : 1}
+          />
+        </div>
         <div className="flex justify-center pb-3 gap-4">
           <Button
             onClick={() => {

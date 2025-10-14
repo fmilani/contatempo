fab6a2b315165963f6199e88cda6ff346b765f7f
Felipe Milani <felmilani@gmail.com>
2025-10-14 19:16:10 -0300

    add @vercel/speed-insights

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <SpeedInsights />
     </html>
   );
 }

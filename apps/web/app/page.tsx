cf9be2d4b5a27c9cac1a992b69ec45672d2decec
Felipe Milani <felmilani@gmail.com>
2025-10-14 19:09:44 -0300

    fix the correct next version

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        Contatempo v3 is comming
+        Contatempo v4 is comming
       </main>
     </div>
   );

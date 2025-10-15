1a91568ba01452dfa1b6f1a3842a5cf003d22266
Felipe Milani <felmilani@gmail.com>
2025-10-15 19:18:32 -0300

    forgot to remove speed-insights usage lol

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,7 +29,6 @@ export default function RootLayout({
       >
         {children}
       </body>
-      <SpeedInsights />
     </html>
   );
 }

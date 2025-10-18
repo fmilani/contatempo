00549ef64b9ffd9efe56f1e3df4f3c5aa3b39974
Felipe Milani <felmilani@gmail.com>
2025-10-18 20:37:01 -0300

    change favicon to ⏱️

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -13,8 +13,15 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Contatempo",
+  title: "contatempo",
   description: "A simple time tracker.",
+  icons: {
+    icon: [
+      {
+        url: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⏱️</text></svg>",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
